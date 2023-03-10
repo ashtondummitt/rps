2d4e11743ab670e190cd789f31a4812d68a6fdc6
ashtondummitt <ashtondummitt@gmail.com>
2023-03-10 00:48:07 -0500

    Make playRound function

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 function getComputerChoice() {
-    let rps = ["rock", "paper", "scissors"];
-    let random = rps[Math.floor(Math.random() * rps.length)];
-    return random;
-}  
+  let rps = ["rock", "paper", "scissors"];
+  let random =
+    rps[Math.floor(Math.random() * rps.length)];
+  return random;
+}
 
 console.log(getComputerChoice());
+
+function playRound(
+  playerSelection,
+  computerSelection
+) {
+  if (playerSelection === computerSelection) {
+    return "Tie Game!";
+  } else if (
+    playerSelection === "rock" &&
+    computerSelection === "paper"
+  ) {
+    return "You lose! Paper beats Rock";
+  } else if (
+    playerSelection === "paper" &&
+    computerSelection === "scissors"
+  ) {
+    return "You lose! Scissors beats Paper";
+  } else if (
+    playerSelection === "scissors" &&
+    computerSelection === "rock"
+  ) {
+    return "You lose! Rock beats Scissors";
+  } else {
+    return "You win!";
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice;
+console.log(
+  playRound(playerSelection, computerSelection)
+);

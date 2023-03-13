d8d510351c2192fd18478dd5e0209e5f34f3711a
ashtondummitt <ashtondummitt@gmail.com>
2023-03-12 22:42:26 -0400

    Rewrite prompt text and impliment options inside input box.

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let playerScore = 0;
 let compScore = 0;
 
-
 function getComputerChoice() {
-  const rps  = ["rock", "paper", "scissors"];
+  const rps = ["rock", "paper", "scissors"];
   let random =
     rps[Math.floor(Math.random() * rps.length)];
   return random;
 }
-
 
 function playRound(
   playerSelection,
@@ -35,33 +33,34 @@ function playRound(
     compScore++;
     return "You lose!";
   } else {
-    playerScore++
+    playerScore++;
     return "You win!";
   }
 }
 
-
-
-
 function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt(
-            'Please choose "rock", "paper", or "scissors".'
-          );
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-        console.log(computerSelection);
-        console.log(playerSelection);
-    }
-    if (playerScore > compScore) {
-        return "You win the game!";
-    } else if (playerScore < compScore) {
-        return "You lose the game!";
-    } else {
-        return "You tied with the computer!"
-    }
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      'I want to play a game...', "Rock, Paper, or Scissors"
+    );
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    console.log(
+      playRound(
+        playerSelection,
+        computerSelection
+      )
+    );
+    console.log(computerSelection);
+    console.log(playerSelection);
+  }
+  if (playerScore > compScore) {
+    return "You win the game!";
+  } else if (playerScore < compScore) {
+    return "You lose the game!";
+  } else {
+    return "You tied with the computer!";
+  }
 }
 
 console.log(game());
-
